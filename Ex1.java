9297929c7fcec0c5ff29f9816143359fb8f679e5
xtir0307 <xtir0307@gmail.com>
2022-02-16 17:26:14 +0700

    Resolve conflict

--- Ex1.java
+++ Ex1.java
@@ -1,6 +1,9 @@
+import java.awt.*;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class Ex1 {
 
@@ -10,16 +13,16 @@ public class Ex1 {
     void Calculator() {
         JFrame frame;
         JTextField text;
-        JButton btn0;
-        JButton btn1;
-        JButton btn2;
-        JButton btn3;
-        JButton btn4;
-        JButton btn5;
-        JButton btn6;
-        JButton btn7;
-        JButton btn8;
-        JButton btn9;
+        JButton btn_0;
+        JButton btn_1;
+        JButton btn_2;
+        JButton btn_3;
+        JButton btn_4;
+        JButton btn_5;
+        JButton btn_6;
+        JButton btn_7;
+        JButton btn_8;
+        JButton btn_9;
         JButton btn_plus;
         JButton btn_minus;
         JButton btn_multi;
@@ -37,26 +40,22 @@ public class Ex1 {
         JButton btn_per;
         JButton btn_over;
 
-        
-
-
-
         frame = new JFrame("Calculator");
 
         text = new JTextField();
         text.setBounds(10, 50, 380, 80);
+        text.setBorder(new LineBorder(Color.LIGHT_GRAY, 8));
 
-
-        btn0 = new JButton("0");
-        btn1 = new JButton("1");
-        btn2 = new JButton("2");
-        btn3 = new JButton("3");
-        btn4 = new JButton("4");
-        btn5 = new JButton("5");
-        btn6 = new JButton("6");
-        btn7 = new JButton("7");
-        btn8 = new JButton("8");
-        btn9 = new JButton("9");
+        btn_0 = new JButton("0");
+        btn_1 = new JButton("1");
+        btn_2 = new JButton("2");
+        btn_3 = new JButton("3");
+        btn_4 = new JButton("4");
+        btn_5 = new JButton("5");
+        btn_6 = new JButton("6");
+        btn_7 = new JButton("7");
+        btn_8 = new JButton("8");
+        btn_9 = new JButton("9");
         btn_mc = new JButton("MC");
         btn_mr = new JButton("MR");
         btn_mp = new JButton("M+");
@@ -72,8 +71,10 @@ public class Ex1 {
         btn_per = new JButton("%");
         btn_over = new JButton("+/-");
         btn_next = new JButton(">");
+        btn_sqrt = new JButton("^");
 
         btn_next.setBounds(330, 150, 60, 30);
+        btn_sqrt.setBounds(250, 150, 60, 30);
         
         btn_mc.setBounds(10, 200, 60, 30);
         btn_mr.setBounds(90, 200, 60, 30);
@@ -81,22 +82,22 @@ public class Ex1 {
         btn_mp.setBounds(250, 200, 60, 30);
         
         btn_per.setBounds(10, 250, 60, 30);
-        btn7.setBounds(90, 250, 60, 30);
-        btn8.setBounds(170, 250, 60, 30);
-        btn9.setBounds(250, 250, 60, 30);
+        btn_7.setBounds(90, 250, 60, 30);
+        btn_8.setBounds(170, 250, 60, 30);
+        btn_9.setBounds(250, 250, 60, 30);
 
         btn_over.setBounds(10, 300, 60, 30);
-        btn4.setBounds(90, 300, 60, 30);
-        btn5.setBounds(170, 300, 60, 30);
-        btn6.setBounds(250, 300, 60, 30);
+        btn_4.setBounds(90, 300, 60, 30);
+        btn_5.setBounds(170, 300, 60, 30);
+        btn_6.setBounds(250, 300, 60, 30);
 
         btn_c.setBounds(10, 350, 60, 30);
-        btn1.setBounds(90, 350, 60, 30);
-        btn2.setBounds(170, 350, 60, 30);
-        btn3.setBounds(250, 350, 60, 30);
+        btn_1.setBounds(90, 350, 60, 30);
+        btn_2.setBounds(170, 350, 60, 30);
+        btn_3.setBounds(250, 350, 60, 30);
 
         btn_ac.setBounds(10, 400, 60, 30);
-        btn0.setBounds(90, 400, 60, 30);
+        btn_0.setBounds(90, 400, 60, 30);
         btn_dot.setBounds(170, 400, 60, 30);
         btn_equals.setBounds(250, 400, 60, 30);
 
@@ -105,41 +106,39 @@ public class Ex1 {
         btn_minus.setBounds(330, 300, 60, 30);
         btn_plus.setBounds(330, 350, 60, 80);
 
-
         frame.setSize(412, 490);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
-
-
-
         
         frame.add(text);
 
         frame.add(btn_next);
+        frame.add(btn_sqrt);
+
         frame.add(btn_mc);
         frame.add(btn_mr);
         frame.add(btn_mm);
         frame.add(btn_mp);
 
         frame.add(btn_per);
-        frame.add(btn7);
-        frame.add(btn8);
-        frame.add(btn9);
+        frame.add(btn_7);
+        frame.add(btn_8);
+        frame.add(btn_9);
 
         frame.add(btn_over);
-        frame.add(btn4);
-        frame.add(btn5);
-        frame.add(btn6);
+        frame.add(btn_4);
+        frame.add(btn_5);
+        frame.add(btn_6);
 
         frame.add(btn_c);
-        frame.add(btn1);
-        frame.add(btn2);
-        frame.add(btn3);
+        frame.add(btn_1);
+        frame.add(btn_2);
+        frame.add(btn_3);
 
         frame.add(btn_ac);
-        frame.add(btn0);
+        frame.add(btn_0);
         frame.add(btn_dot);
         frame.add(btn_equals);
 
